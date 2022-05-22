8bb0ebec2ed49d4bd2184a716b805a684c40510e
cch <aehtla2012@gmail.com>
2022-05-22 17:23:50 +0800

    model added

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/webSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.udacity.jwdnd.course1.cloudstorage.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class webSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().formLogin().disable();  // <-- stop default login-in page
+    }
+
+}
