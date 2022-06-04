b07fd42f2b662c9ba997f2365331f820855e2989
cch <aehtla2012@gmail.com>
2022-06-04 21:00:12 +0800

    security mechanism added

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/homeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/homeController.java
@@ -1,16 +1,24 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class homeController {
 
-    @GetMapping("/home")
+    @GetMapping
     public String homePage(){
         return "home";
+    }
+
+    @PostMapping("/logout")
+    public String backLogin(Model model){
+        model.addAttribute("logOutStatus",true);
+        return "login";
     }
 
 }
--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/loginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/loginController.java
@@ -20,6 +20,8 @@ public class loginController {
 
     @GetMapping
     public String login(){
+
+
         return "login";
     }
 
