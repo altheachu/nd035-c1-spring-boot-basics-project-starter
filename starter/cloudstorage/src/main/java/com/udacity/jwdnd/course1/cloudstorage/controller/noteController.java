9270f8c30c783eac35189e5d0e2e0ede6d269aff
cch <aehtla2012@gmail.com>
2022-06-05 16:01:21 +0800

    note deleted 1st success

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/noteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/noteController.java
@@ -32,7 +32,7 @@ public class noteController {
         // confirm the user id
         User user = userService.getUser(authentication.getName());
         Integer userId = user.getUserId();
-        Integer noteId = note.getNoteId();
+        Integer noteId = note.getNoteId();// string<-->integer cast problem
         Note dbNote = noteService.getNote(noteId);
 
         if(dbNote==null){
