cb1661fa2d6fef161b67a542b0c8d329e9c714f1
cch <aehtla2012@gmail.com>
2022-06-05 10:32:48 +0800

    note added 1st success

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/homeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/homeController.java
@@ -1,5 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.noteServiceImpl;
+import com.udacity.jwdnd.course1.cloudstorage.services.userServiceImpl;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class homeController {
 
+    noteServiceImpl noteService;
+    userServiceImpl userService;
+
+    public homeController(noteServiceImpl noteService, userServiceImpl userService) {
+        this.noteService = noteService;
+        this.userService = userService;
+    }
+
     @GetMapping
-    public String homePage(){
+    public String homePage(Model model, Authentication authentication){
+
+        User user = userService.getUser(authentication.getName());
+        model.addAttribute("clientNotes",noteService.getNote(user.getUserId()));
+
         return "home";
     }
 
